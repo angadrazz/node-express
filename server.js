7157974b2490e28bec944c50b15321176b99afbb
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-09 11:14:35 +0530

    srver js

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
 
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// app.use(express.static("public"))
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
 
-app.set("view engine", "ejs")
+// app.set("view engine", "ejs")
 
-const userRouter = require("./routes/users")
+// const userRouter = require("./routes/users")
 
-app.use("/users", userRouter)
+// app.use("/users", userRouter)
 
-app.listen(3000)
-console.log("hello")
+// app.listen(3000)
+// console.log("hello")
+
+let x = 15;
+let y = 54;
+console.log(x <= y)
